c01d6ebcfa8bee527dccf8dc5deb6898ac2ad827
MikeMaloney77 <36928523+MikeMaloney77@users.noreply.github.com>
2018-03-04 21:26:30 -0600

    Update Cards.py
    I tried creating a deck I am like 67% sure if this works

--- Cards.py
+++ Cards.py
@@ -1,85 +1,46 @@
+from Cards import *
 import random
 
-class Card(object):
-    """Create a card base on rank and suit"""
-    RANKS = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
-    SUITS = ["S","C","H","D"]
+class Deck(object):
+    """Creates the deck"""
 
-    def __init__(self, rank:str, suit:str, isFaceUp = True):
-        self.rank = rank
-        self.suit = suit
-        self.card = (rank, suit)
-        self.isFaceUp = isFaceUp
+
+    def __init__(self, cardCounter):
+        """this is like a counter almost"""
+        self.cardCounter = cardCounter
+
 
     def __str__(self):
-        if self.isFaceUp:
-            output = self.rank + "-" + self.suit
-            return output
-        else:
-            return "XX"
-
-    def flipCard(self):
-        if self.isFaceUp:
-            self.isFaceUp = False
-        else:
-            self.isFaceUp = True
+        """this is the info"""
+        info = "Number of cards in the deck" + str(self.cardCounter)
 
 
-class Hand(object):
-    """This creates a hand of cards"""
-
-    def __init__(self):
-        """creates an empty hand"""
-        self.cards = []
-
-    def __str__(self):
-        if len(self.cards) == 0:
-            return "<Empty Hand>"
-        output = ""
-        for card in self.cards:
-            output += "[" +card.__str__() + "] "
-        return output
-
-    def addCard(self,card:Card):
-        """This will add a card to the hand"""
-        self.cards.append(card)
-
-    def giveCard(self, card:Card, otherHand):
-        """removes card from current hand
-        to add to other hand"""
-        if card in self.cards:
-            otherHand.addCard(card)
-            self.cards.remove(card)
-        else:
-            print("can't give that card")
-
-    def getCard(self, rank, suit):
-        """return a card from the hand of a given
-        rank and suit: None otherwise"""
-        for card in self.cards:
-            if card.rank == rank and card.suit == suit:
-                return card
-        return None
-
-    def clearCards(self):
-        """remove all cards from hand"""
-        self.cards = []
+    def get_cardCounter(self):
+        """Counts the number of cards in a deck"""
 
 
-h = Hand()
-otherHand = Hand()
-h.addCard(Card("9","D"))
-h.addCard(Card("J","D"))
-print(h)
-h.giveCard(h.getCard("J","C"), otherHand)
-print(h)
-print(otherHand)
-h.clearCards()
-print(h)
+    def createDeck(self):
+        """Creates a deck of cards"""
+        cardz = []
+        cardCounter = 0
+        for i in Card.SUITS:
+            for j in Card.RANKS:
+                card = Card(j, i)
+                cardz += card
+                cardCounter += 1
+        return cardz
 
 
-
-
+hand1 = Hand()
+deck1 = Deck(52)
+deck1.createDeck()
+hand1.addCard(deck1)
+print(hand1)
+print(deck1)
+#self.createDeck(deck1 = Deck(52))
+#print(deck1)
+#Deck.createDeck()
+#print(Deck)
 
 
 
